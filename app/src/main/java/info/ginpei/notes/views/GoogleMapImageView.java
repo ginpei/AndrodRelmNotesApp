f9b94f0be880be6db0e0c473eebb5ddd449f66fa
ginpei <ginpei@gmail.com>
2017-03-23 04:02:12 -0700

    reduce updating

--- app/src/main/java/info/ginpei/notes/views/GoogleMapImageView.java
+++ app/src/main/java/info/ginpei/notes/views/GoogleMapImageView.java
@@ -9,6 +9,7 @@ import android.location.Location;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,17 +19,31 @@ public class GoogleMapImageView extends android.support.v7.widget.AppCompatImage
 
     public static final int WIDTH = 300;
     public static final int HEIGHT = 300;
-    private Location location;
+    public static final int THRESHOLD = 10;
+    public static final String TAG = "G#GoogleMapImageView";
+    private double latitude = -1;
+    private double longitude = -1;
 
     public GoogleMapImageView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
     public void setLocation(Activity activity, Location location) {
-        this.location = location;
+        double newLatitude = location.getLatitude();
+        double newLongitude = location.getLongitude();
+
+        double diff = Math.max(Math.abs(newLatitude - latitude), Math.abs(newLongitude - longitude));
+        boolean isNew = latitude < 0;
+        if (!isNew && diff < THRESHOLD) {
+            Log.d(TAG, "setLocation: Diff is too small to update: " + diff);
+            return;
+        }
+
+        latitude = newLatitude;
+        longitude = newLongitude;
 
         new Thread(() -> {
-            String url = buildImageUrl(location);
+            String url = buildImageUrl(latitude, longitude);
             Bitmap bitmap = loadBitmap(url);
 
             activity.runOnUiThread(() -> {
@@ -38,13 +53,13 @@ public class GoogleMapImageView extends android.support.v7.widget.AppCompatImage
     }
 
     @SuppressLint("DefaultLocale")
-    private String buildImageUrl(Location location) {
+    private String buildImageUrl(double latitude, double longitude) {
         return String.format(
                 "https://maps.googleapis.com/maps/api/staticmap?size=%dx%d&scale=2&markers=%s,%s",
                 WIDTH,
                 HEIGHT,
-                location.getLatitude(),
-                location.getLongitude()
+                latitude,
+                longitude
         );
     }
 
